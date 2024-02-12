60508fa6ad22656c027396a4bc23df18da7ae846
Saud Ahmed <mahmedsaud@gmail.com>
2024-02-12 15:57:59 +0100

    Populate Detail screen with data

--- src/screens/dropsDetail/DropsDetail.tsx
+++ src/screens/dropsDetail/DropsDetail.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import {ScrollView, Image, StyleSheet} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {useTheme} from 'assets/createTimelessTheme';
 import SafeAreaContainer from 'src/components/safeAreaContainer/SafeAreaContainer';
@@ -7,6 +7,17 @@ import {useAsset} from '../../../queries';
 import {RootStackParamList} from 'src/types/rootStackParams';
 import {Loading} from 'src/components/loading/Loading';
 import {Error} from 'src/components/error/Error';
+import {Box} from 'src/components/box/Box';
+import {Text} from 'src/components/text/Text';
+
+const styles = StyleSheet.create({
+  image: {
+    height: 220,
+    width: '60%',
+    resizeMode: 'cover',
+    alignSelf: 'center',
+  },
+});
 
 const DropsDetail = ({
   navigation,
@@ -26,8 +37,22 @@ const DropsDetail = ({
 
   return (
     <SafeAreaContainer edges={['top']}>
-      <ScrollView style={{flex: 1}}>
-        <Text>{JSON.stringify(data?.asset)}</Text>
+      <ScrollView
+        style={{
+          backgroundColor: data?.asset.heroColour || 'transparent',
+        }}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: data?.asset.heroImage ? data?.asset.heroImage : undefined,
+          }}
+        />
+        <Text variant="h4" fontWeight={'300'}>
+          {data?.asset.label}
+        </Text>
+
+        <Text variant="captions1">{data?.asset.actualPrice}</Text>
+        <Text variant="captions1">{data?.asset.price}</Text>
       </ScrollView>
     </SafeAreaContainer>
   );
